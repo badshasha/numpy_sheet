221324366f2b60d233bdf088b663b9a63799c529
shavendra <shavendragoesoft@gmail.com>
2022-02-27 17:46:49 +0530

    advance slicing

--- file2.py
+++ file2.py
@@ -4,7 +4,7 @@ import numpy as np
 a = np.array([1,2])
 print(a.shape)
 
-a = np.array( [[1,2], [3,4]] )
+a = np.array( [[1,2,4,5], [3,4,1,2]] )
 print(a.shape)  # (2,2)  row , column
 
 
@@ -15,4 +15,11 @@ print( a[0,0] )
 print( a[:,0] )
 print( a[0,:] )
 
-print(a.T)
+
+
+# more slicing
+print( a[0,1:3] )  # start inedx 1 and end 2
+
+
+
+print(  a[-1,-1] )  # start last row and last coulumd 
