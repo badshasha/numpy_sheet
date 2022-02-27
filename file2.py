6df2a09bc416fdd6e1fa02fc9d380cce87513de2
shavendra <shavendragoesoft@gmail.com>
2022-02-27 21:41:26 +0530

    concatination

--- file2.py
+++ file2.py
@@ -1,28 +1,33 @@
 import numpy as np
 
+a  = np.array([1,2,3,4])
 
-a = np.array([1, 2, 3, 4, 5])
+# append element
+b  = np.array([5,6,7,8])
 
-
-# createing and rearange
-
-b = np.arange( 1,11 )
-print(b)
-print(b.shape)
-
-# reshaping
-
-c = b.reshape((2,5))
+c = np.concatenate([a, b])
 print(c)
-print(c.shape)
 
-# another shape
-d = b.reshape((5,2))
-print(d)
-print(d.shape)
 
-# list of list
 
-e = b[np.newaxis , :]
-print(e)
-print(e.shape)
+# two dementional
+
+array1 = np.array( [[1,2],[3,4]] )
+array2 = np.array( [[5,6]] )
+
+array3 = np.concatenate([array1, array2])
+print(array3)   # [[1 2]
+                 # [3 4]
+                 # [5 6]]
+
+
+array3 = np.concatenate([array1, array2] , axis=None)  # no dementional array shown
+print(array3)  # [1 2 3 4 5 6]
+
+# error
+array3 = np.concatenate([array1, array2] , axis=1)  # because demention not matching
+
+
+# transpose
+array3 = np.concatenate([array1, array2.T] , axis=1)
+print(array3)
