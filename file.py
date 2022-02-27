77b9c3559133e90f8384a7d005dd49de0374aa6f
shavendra <shavendragoesoft@gmail.com>
2022-02-27 14:07:58 +0530

    introduction

---
+++ file.py
@@ -0,0 +1,18 @@
+
+# most power full scirntific computing lib for python
+# user for
+    # sckikit -learning , matplotlib , pandas ,
+
+# easy to workes with high performance mulitdimensional array
+# mathamarical operations with array is easy
+# most of the back end code writen using c language there or operation fast compaer to other libra
+
+
+# usrcases
+
+ ## array operation
+ # dor product
+ # matrix multiplicaiton
+ # liner systems
+ # lnverse , determinant
+ 
