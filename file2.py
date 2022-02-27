7178f45d4fde8a13986b840286abb2360161ea60
shavendra <shavendragoesoft@gmail.com>
2022-02-27 21:44:46 +0530

    concatinate with H and V stack

--- file2.py
+++ file2.py
@@ -1,33 +1,17 @@
 import numpy as np
 
-a  = np.array([1,2,3,4])
 
-# append element
-b  = np.array([5,6,7,8])
 
-c = np.concatenate([a, b])
+a = np.arange(1,5)
+b = np.arange(5,9)
+
+
+#hstack   horizontal stack
+
+c =np.hstack((a,b))
 print(c)
 
 
-
-# two dementional
-
-array1 = np.array( [[1,2],[3,4]] )
-array2 = np.array( [[5,6]] )
-
-array3 = np.concatenate([array1, array2])
-print(array3)   # [[1 2]
-                 # [3 4]
-                 # [5 6]]
-
-
-array3 = np.concatenate([array1, array2] , axis=None)  # no dementional array shown
-print(array3)  # [1 2 3 4 5 6]
-
-# error
-array3 = np.concatenate([array1, array2] , axis=1)  # because demention not matching
-
-
-# transpose
-array3 = np.concatenate([array1, array2.T] , axis=1)
-print(array3)
+#vstack  virttically stack
+d   = np.vstack((a,b))
+print(d)
