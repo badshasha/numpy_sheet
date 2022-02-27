149eb4389bc876f01cf6cd57b0d57783445e2e95
shavendra <shavendragoesoft@gmail.com>
2022-02-27 18:09:49 +0530

    fancing indexing [+] important

--- file2.py
+++ file2.py
@@ -4,6 +4,15 @@ import numpy as np
 a = np.array([1, 2, 3, 4, 5])
 print(a)
 
-# fanci indexing 
+# fanci indexing
 b = [1,2,3]
 print(a[b])
+
+
+c  =np.argwhere(a%2==0).flatten() # indexing
+print(c) # indexing
+
+print(f'argwhere  {a[c]}')
+
+
+print(f'other way { a[a%2==0] }')
